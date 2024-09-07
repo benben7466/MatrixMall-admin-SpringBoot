accc9e5d05d749d1066658e46f7a0c88904dd32c
benben7466 <benben7466@qq.com>
2024-09-07 16:36:19 +0800

    完善部门管理

---
+++ src/main/java/com/bzq/matrixmall/common/constant/SymbolConstant.java
@@ -0,0 +1,7 @@
+package com.bzq.matrixmall.common.constant;
+
+//符号和特殊符号常用类
+public interface SymbolConstant {
+    //符号：逗号
+    String COMMA = ",";
+}
--- src/main/java/com/bzq/matrixmall/service/SysDeptService.java
+++ src/main/java/com/bzq/matrixmall/service/SysDeptService.java
@@ -3,6 +3,10 @@ package com.bzq.matrixmall.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.bzq.matrixmall.common.model.Option;
 import com.bzq.matrixmall.model.entity.SysDept;
+import com.bzq.matrixmall.model.form.DeptForm;
+import com.bzq.matrixmall.model.query.DeptQuery;
+import com.bzq.matrixmall.model.vo.DeptVO;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -10,4 +14,19 @@ import java.util.List;
 public interface SysDeptService extends IService<SysDept> {
     //部门树形下拉选项
     List<Option<Long>> listDeptOptions();
+
+    //部门列表
+    List<DeptVO> getDeptList(DeptQuery queryParams);
+
+    //新增部门
+    Long saveDept(DeptForm formData);
+
+    //删除部门
+    boolean deleteByIds(String ids);
+
+    //获取部门详情
+    DeptForm getDeptForm(Long deptId);
+
+    //修改部门
+    Long updateDept(Long deptId, DeptForm formData);
 }
